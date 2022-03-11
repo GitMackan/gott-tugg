87f41a2f3a39923ad94ce5d40e654f6ee80e63af
Marcus Nygård <mackanspel123@hotmail.com>
2022-03-11 21:45:04 +0100

    test

--- src/container/Menu/DrinkMenu.js
+++ src/container/Menu/DrinkMenu.js
@@ -17,7 +17,7 @@ const DrinkMenu = () => {
   }, [])
   
   const getVineBeer = async () => {
-    const response = await axios.get("https://admingottugg.azurewebsites.net/api/vinebeerapi");
+    const response = await axios.get("/vinebeerapi");
     if(response.status === 200) {
       setVineBeer(response.data);
       console.log(vineBeer);
